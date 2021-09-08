d9e8d887a81f2dc37bde5c1f4a3022c7814d45ec
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-09-09 02:40:10 +0600

    scanning test file

---
+++ algo-implementation/scan.sh
@@ -0,0 +1,53 @@
+echo "Enter the number of input n:"
+read n
+echo "enter the nuumber of request"
+for((i=1;i<=n;i++))
+do
+read rq[$i]
+done
+for((i=1;i<=n-1;i++))
+do
+for((j=1;j<=n-i;j++))
+do
+if ((rq[$j]> ${rq[$j+1]}))
+then
+temp=${rq[$j]}
+rq[j]=${rq[j+1]}
+rq[j+1]=$temp
+fi
+done 
+done
+##sorting
+echo "please enter the current position "
+read curr
+
+tot1=0
+tot2=0
+echo "the track position "
+read track
+for((i=1;i<=n;i++))
+do
+if(($curr<${rq[$i]}))
+then
+sub=$((${rq[$i]}-$curr))
+tot1=$(($tot1+$sub))
+curr=${rq[$i]}
+fi
+
+done
+p1=$(($track-$curr))
+tot1=$(($tot1+$p1))
+echo "please enter the current position "
+read curr1
+for((i=n;i>=1;--i))
+do
+if(($curr1>${rq[$i]}))
+then
+sub=$(($track-${rq[$i]}))
+tot2=$(($tot2+$sub))
+track=${rq[$i]}
+fi
+done 
+
+total=$(($tot1+$tot2))
+echo "the total seek time $total"
